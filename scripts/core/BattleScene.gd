21f4c035842e100121a7ba44d77cb031d6fd2bc5
xiaohuangji2020 <jenrya2020@gmail.com>
2025-07-13 23:51:16 +0800

    init

--- scripts/core/BattleScene.gd
+++ scripts/core/BattleScene.gd
@@ -3,23 +3,61 @@ extends Node2D
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 @onready var beast_base: Node2D = $TileMapLayer/BeastBase
 
-var selected_beast = null
 
 func _ready() -> void:
-	beast_base.selected.connect(on_beast_selected)
-
 	var grid_pos = Vector2i(2,5)
 	var pixel_pos = tile_map_layer.map_to_local(grid_pos)
 	beast_base.position = pixel_pos
 
-func on_beast_selected(beast_instance) -> void:
-	print('信号：', beast_instance.name)
-	selected_beast = beast_instance
+#func _unhandled_input(event: InputEvent) -> void:
+	#if selected_beast != null and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			#var target_grid_pos = tile_map_layer.local_to_map(event.position)
+			#var target_pixel_pos = tile_map_layer.map_to_local(target_grid_pos)
+			#selected_beast.position = target_pixel_pos
+			#print('移动到格子', target_grid_pos)
+			#selected_beast = null
+			#get_viewport().set_input_as_handled()
 
-func _unhandled_input(event: InputEvent) -> void:
-	if selected_beast != null and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var target_grid_pos = tile_map_layer.local_to_map(event.position)
+var selected_beast = null
+func _input(event: InputEvent) -> void:
+	# 不是左键直接返回
+	if not (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+		return
+	# 获取物理世界并准备射线查询
+	var space_state = get_world_2d().direct_space_state
+	var mouse_pos = get_global_mouse_position()
+	# 设置查询参数，获取在鼠标位置的所有碰撞题
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = mouse_pos
+	# 指定碰撞掩码，精灵都在第2个物理层
+	query.collision_mask = 2
+	# 查询默认不包含area2D
+	query.collide_with_areas = true
+
+	var results = space_state.intersect_point(query)
+	# 分析查询结果
+	if results.is_empty():
+		# 情况A：点击到了空白区域
+		if selected_beast != null:
+			var target_grid_pos = tile_map_layer.local_to_map(mouse_pos)
 			var target_pixel_pos = tile_map_layer.map_to_local(target_grid_pos)
 			selected_beast.position = target_pixel_pos
 			print('移动到格子', target_grid_pos)
+			selected_beast.get_node("Sprite2D").modulate = Color.WHITE
 			selected_beast = null
+	else:
+		# 情况B：点击到了一个或多个精灵
+		var top_beast = null
+		var max_y = -INF
+		for result in results:
+			var beast = result.collider.get_owner() #获取碰撞器的容器area2D，再找area2D的owner 即beast的根节点
+			# Y值越大，在屏幕上越靠下，视觉上越靠前
+			if beast.global_position.y > max_y:
+				max_y = beast.global_position.y
+				top_beast = beast
+		if top_beast != null:
+			if selected_beast != null:
+				selected_beast.get_node("Sprite2D").modulate = Color.WHITE
+			selected_beast = top_beast
+			selected_beast.get_node("Sprite2D").modulate = Color.BLUE
+			print("选中了精灵：", selected_beast.name)
--- scripts/sprites/BeastBase.gd
+++ scripts/sprites/BeastBase.gd
@@ -1,7 +1,1 @@
 extends Node2D
-
-signal selected(sprites_instance)
-
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		emit_signal("selected", self)
