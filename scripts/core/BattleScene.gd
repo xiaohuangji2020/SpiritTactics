fe73550de7accb780455cb8d8b0eb80a067c98f8
xiaohuangji2020 <jenrya2020@gmail.com>
2025-07-13 22:39:51 +0800

    init

--- scripts/core/BattleScene.gd
+++ scripts/core/BattleScene.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
-@onready var sprite_base: Node2D = $TileMapLayer/SpriteBase
+@onready var beast_base: Node2D = $TileMapLayer/BeastBase
+
+var selected_beast = null
 
 func _ready() -> void:
-	var grid_pos = Vector2i(2,2)
+	beast_base.selected.connect(on_beast_selected)
+
+	var grid_pos = Vector2i(2,5)
 	var pixel_pos = tile_map_layer.map_to_local(grid_pos)
-	sprite_base.position = pixel_pos
+	beast_base.position = pixel_pos
+
+func on_beast_selected(beast_instance) -> void:
+	print('信号：', beast_instance.name)
+	selected_beast = beast_instance
+
+func _unhandled_input(event: InputEvent) -> void:
+	if selected_beast != null and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var target_grid_pos = tile_map_layer.local_to_map(event.position)
+			var target_pixel_pos = tile_map_layer.map_to_local(target_grid_pos)
+			selected_beast.position = target_pixel_pos
+			print('移动到格子', target_grid_pos)
+			selected_beast = null
---
+++ scripts/sprites/BeastBase.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+signal selected(sprites_instance)
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		emit_signal("selected", self)
